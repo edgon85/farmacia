f4e580006b9557cf2d3dd982ac74db32cdb145b4
edgon <edgon85@gmail.com>
2019-12-13 17:43:45 -0600

    dando funcionalidad a buttonbar con sus propias pages

--- lib/src/pages/container_page.dart
+++ lib/src/pages/container_page.dart
@@ -1,3 +1,6 @@
+import 'package:farm_app/src/pages/categories/category_page.dart';
+import 'package:farm_app/src/pages/favorites/favorites_page.dart';
+import 'package:farm_app/src/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class ContainerPage extends StatefulWidget {
@@ -8,28 +11,25 @@ class ContainerPage extends StatefulWidget {
 class _ContainerPageState extends State<ContainerPage> {
   int _currentIndex = 0;
 
+  final _home = HomePage();
+  final _category = CategoryPage();
+  final _favorites = FavoritePage();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      /*<============================>*/
-      /*Appbar*/
-      /*<============================>*/
-      appBar: AppBar(
-        title: Text('Parmapp'),
-      ),
-      /*>============================<*/
 
       /*==============================*/
       /*Body*/
       /*==============================*/
-      body: Container(
-        child: Center(
-          child: Text('Home page'),
-        ),
-      ),
+      body: _pageChooser(_currentIndex),
       /*==============================*/
 
+      /*<============================>*/
+      /*Bottom Bar*/
+      /*<============================>*/
       bottomNavigationBar: _bottomBar(),
+      /*>============================<*/
     );
   }
 
@@ -37,12 +37,43 @@ class _ContainerPageState extends State<ContainerPage> {
   /*Bottom Bar*/
   /*==============================*/
   Widget _bottomBar() {
-    return BottomNavigationBar(elevation: 4, items: <BottomNavigationBarItem>[
-      BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Inicio')),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.category), title: Text('Categorías')),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite), title: Text('Favoritos')),
-    ]);
+    return BottomNavigationBar(
+      elevation: 4,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Inicio')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.category), title: Text('Categorías')),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite), title: Text('Favoritos')),
+      ],
+      currentIndex: _currentIndex,
+      onTap: (value) {
+        setState(() {
+          _currentIndex = value;
+        });
+      },
+    );
+  }
+
+  /*==============================*/
+  /* Seleccionar la página para el bottom bar */
+  /*==============================*/
+  Widget _pageChooser(int currentIndex) {
+    switch (currentIndex) {
+      case 0:
+        return _home;
+        break;
+
+      case 1:
+        return _category;
+        break;
+
+      case 2:
+        return _favorites;
+        break;
+
+      default:
+        return _home;
+    }
   }
 }
