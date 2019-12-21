d741968e6d77febf0edd9e988a552b87f955cfc1
edgon <edgon85@gmail.com>
2019-12-20 22:01:05 -0600

    terminado el formulario de ingreso (login)

--- lib/src/pages/acounts/login_page.dart
+++ lib/src/pages/acounts/login_page.dart
@@ -14,39 +14,145 @@ class _LoginPageState extends State<LoginPage> {
         body: Stack(
           children: <Widget>[
             _fondo(context),
-            _loginForm(context),
+            _loginContainer(context),
           ],
         ),
       ),
     );
   }
 
-  Widget _loginForm(BuildContext context) {
+  Widget _loginContainer(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          SafeArea(child: Container(
-            height: size.height * 0.25,
+          SafeArea(
+              child: Container(
+            height: size.height * 0.20,
           )),
-
+          /*>===============================================<*/
+          /*Contenedor de formulario*/
+          /*>===============================================<*/
           Container(
             width: size.width * 0.85,
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
             decoration: BoxDecoration(
-              color: Colors.white
-            ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3.0,
+                      offset: Offset(0.0, 5.0),
+                      spreadRadius: 3.0),
+                ]),
             child: Column(
               children: <Widget>[
-                Text('Ingreso')
+                /*>===============================================<*/
+                /*Texto de ingreso*/
+                /*>===============================================<*/
+                Text(
+                  'Ingreso',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                /*<===============================================>*/
+                SizedBox(
+                  height: 40.0,
+                ),
+                /*>===============================================<*/
+                /*inputs del login*/
+                /*>===============================================<*/
+                _emailInput(),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _passwordInput(),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _buttonLogin(),
+                /*<===============================================>*/
               ],
             ),
-          )
+          ),
+
+          /*>===============================================<*/
+          /*Crear cuenta, recuperar contraseña*/
+          /*>===============================================<*/
+          Text('Crear cuenta'),
+          SizedBox(
+            height: 40.0,
+          ),
+          Text('¿Olvido la contraseña?'),
+          /*>===============================================<*/
+
+          SizedBox(
+            height: 50.0,
+          ) // <= espacio en el botton para hacer scroll
         ],
       ),
     );
   }
 
+  /*>===============================================<*/
+  /*email input login*/
+  /*>===============================================<*/
+  Widget _emailInput() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: ColorApp.accentColor),
+          hintText: 'correo@tucorreo.com',
+          labelText: 'Correo electrónico',
+        ),
+      ),
+    );
+  }
+  /*<===============================================>*/
+
+  /*>===============================================<*/
+  /*password input login*/
+  /*>===============================================<*/
+  Widget _passwordInput() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: ColorApp.accentColor),
+          labelText: 'Contraseña',
+        ),
+      ),
+    );
+  }
+  /*<===============================================>*/
+
+  /*>===============================================<*/
+  /*button login*/
+  /*>===============================================<*/
+  Widget _buttonLogin() {
+    return RaisedButton(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+          child: Text('Ingresar'),
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        elevation: 0.0,
+        textColor: Colors.white,
+        color: ColorApp.accentColor,
+        onPressed: () {});
+  }
+  /*<===============================================>*/
+
+  /*>===============================================<*/
+  /*Fondo del login*/
+  /*>===============================================<*/
   Widget _fondo(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
@@ -98,7 +204,6 @@ class _LoginPageState extends State<LoginPage> {
         ],
       ),
     );
-
 
     return Stack(
       children: <Widget>[
